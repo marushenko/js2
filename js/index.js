401906e518cdbd9d63eff9bcde85abdf05fd5d23
ntguilty <jura.marushenko@gmail.com>
2018-10-28 19:59:28 +0000

    Created initial files

--- js/index.js
+++ js/index.js
@@ -6,12 +6,17 @@ const c = parseFloat(prompt('Enter C'));
 function solveQuadrEq(a, b, c) {
     const d = diskr(a, b, c);
     if (d > 0) {
-        const x1 = (-b+Math.sqrt(d))/(2*a);
-        const x2 = (-b-Math.sqrt(d))/(2*a);
-        return 
+        const x1 = (-b + Math.sqrt(d)) / (2 * a);
+        const x2 = (-b - Math.sqrt(d)) / (2 * a);
+        alert('x1=' + x1 + 'x2 =' + x2);
+    } else if (d == 0) {
+        const x1 = (-b) / (2 * a);
+        alert('x =' + x1);
+    } else if (d < 0) {
+        alert('No solution');
     }
-    }
-    
+}
+ 
     
 
 
@@ -19,5 +24,5 @@ function diskr(a, b, c) {
     return (b * b) - (4 * a * c);
 }
 
-alert(diskr(a, b, c));
+alert(solveQuadrEq(a, b, c));
 
